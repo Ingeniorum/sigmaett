caffb333986fb031e7069be2e67cd727a6a6a1a9
wipmac <drudruman@hotmail.com>
2014-05-21 19:21:00 +0100

    Modifications

--- sigmaett-front-end/src/main/java/com/ingeniorum/sigmaett/HomeController.java
+++ sigmaett-front-end/src/main/java/com/ingeniorum/sigmaett/HomeController.java
@@ -3,11 +3,14 @@ package com.ingeniorum.sigmaett;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -21,14 +24,22 @@ public class HomeController
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model)
     {
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-                DateFormat.LONG, locale);
-
-        String formattedDate = dateFormat.format(date);
-
-        model.addAttribute("serverTime", formattedDate);
-
+        model.addAttribute("serverTime", "Nothing");
         return "home";
+    }
+
+    @RequestMapping(value = "/ajaxtest", method = RequestMethod.POST)
+    public @ResponseBody String getTime(@RequestParam("field1") String field1,
+            @RequestParam("field2") String field2)
+    {
+        Random rand = new Random();
+        float r = rand.nextFloat() * 100;
+        String result = "<br>Next Random # is <b>" + r
+                + "</b>. Generated on <b>" + new Date().toString()
+                + " field1 = " + field1 + " field2 = " + field2 + "</b>";
+        System.out
+                .println("Debug Message from CrunchifySpringAjaxJQuery Controller.."
+                        + new Date().toString());
+        return result;
     }
 }
--- sigmaett-front-end/src/main/webapp/WEB-INF/views/home.jsp
+++ sigmaett-front-end/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,42 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
+
+<script type="text/javascript"
+	src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
+<script type="text/javascript">
+	function crunchifyAjax() {
+		$.ajax({
+			url : 'ajaxtest',
+			type : 'POST',
+			data : {
+				field1 : $("#firstname").val(),
+				field2 : "hello2"
+			},
+			success : function(data) {
+				$('#result').html(data);
+			}
+		});
+	}
+
+	$(document).ready(function() {
+		$("#boton").click(function() {
+			crunchifyAjax();
+		});
+	});
+</script>
+
 <html>
 <head>
-	<title>Home</title>
+
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<h1>Hello world!</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+	<input type="text" id="firstname" name="firstname" />
+	<button id="boton">Get External Content</button>
+
+	<div id="result" align="center">Nothing yet!</div>
 </body>
 </html>
